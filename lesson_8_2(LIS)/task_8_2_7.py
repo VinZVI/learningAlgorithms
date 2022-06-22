653217b2cdf564e71c3524dca72d4df5950fbfbb
Михаил <Vinilzm@gmail.com>
2022-06-22 23:05:54 +0300

    lesson 8_2_6(LNiS) time limit exceeded

--- lesson_8_2(LIS)/task_8_2_7.py
+++ lesson_8_2(LIS)/task_8_2_7.py
@@ -1,14 +1,34 @@
+import cProfile
+
+
 def lis_funtion(n, array_numbers):
-    d = []
+    d, prev = [], []
     for i in range(0, n):
         d.append(1)
+        prev.append(-1)
         for j in range(0, i):
             if array_numbers[j] >= array_numbers[i] and d[j] + 1 > d[i]:
                 d[i] = d[j] + 1
+                prev[i] = j
     ans = 0
     for i in range(0, n):
         ans = max(ans, d[i])
-    return ans
+
+    return f"{ans}\n{array_numbers}\n{d}\n{prev}\n{restor_result(ans, d, prev)}"
+
+
+def restor_result(ans, d, prev):
+    l = [1 for _ in range(ans)]
+    k = 0
+    for i in range(1, len(d)):
+        if d[i] > d[k]:
+            k = i
+    j = ans - 1
+    while k > 0:
+        l[j] = k + 1
+        j -= 1
+        k = prev[k]
+    return l
 
 
 def input_file():
@@ -34,5 +54,5 @@ def main():
 
 if __name__ == "__main__":
     # start_time = time.time()
-    main()
+    cProfile.run(main())
     # print(time.time() - start_time)
